42399c0b85cb1cf78a45d97a70739c40dd022023
Antonio Astudillo <antonioastudillo206@gmail.com>
2024-05-30 21:17:39 -0600

    Cambiando imagen de login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,13 +20,13 @@
 </head>
 <body style="background-image: url({{ asset('img/EVALUACIÓN1.png') }}); background-size: cover; background-position: center; background-repeat: no-repeat;">
     <main class="container vh-100">
-        <div class="container py-3">
+        <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col col-xl-10">
                     <div class="card" style="border-radius: 1rem;">
                         <div class="row g-0">
                             <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp" alt="Login de autenticación" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
+                                <img src="{{ asset('img/logo.jpg') }}" alt="Login de autenticación" class="" style="border-radius: 1rem 0 0 1rem; max-width: 100%; height: 100%;" >
                             </div>
                             <div class="col-md-6 col-lg-7 d-flex align-items-center">
                                 <div class="card-body p-4 p-lg-5 text-black">
--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/', function () {
 
 Auth::routes(
     [
-        'register' => false,
+        'register' => true,
         'reset' => false,
     ]
 
